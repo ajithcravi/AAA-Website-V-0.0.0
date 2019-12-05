50cad2a5c6f507e7b1c326c9a1e9a3cc96a37470
ajithcravi <craviajith@gmail.com>
2019-12-05 18:14:33 +0530

    changes added

--- js/projectTile.js
+++
@@ -1,9 +0,0 @@
-// This flag is declared to check whether number of times 'addImagesToCarousel' funnction is run. If it is the first time, active class should be added to the dots and carousel images
-let flag = 0;
-
-//Fetch the details of the images to be used in main carousel from the cloudinary cdn
-fetch(
-  "http://res.cloudinary.com/ajithvault/raw/upload/v1575472700/AAA/Data/carouselImages_kzwtiy.json"
-)
-  .then(response => response.json())
-  .then(data => localStorage.setItem("carouselData", JSON.stringify(data)));
---
+++ js/projects.js
@@ -0,0 +1,15 @@
+const projectDetails = [
+  {
+    projectName: "Mr & Mrs C.M Reddy Home",
+    client: "Mr C.M Reddy",
+    location: "Bangalore",
+    builtArea: 6000,
+    year: 2019,
+    images: [
+      "https://res.cloudinary.com/ajithvault/image/upload/v1575547114/BLR_CM%20Reddy/_BLR_CM_Reddy_Res_Int_01_Home_zctiow.jpg",
+      "https://res.cloudinary.com/ajithvault/image/upload/v1575547105/BLR_CM%20Reddy/BLR_CM_Reddy_Res_Int_00_oat8tg.jpg",
+    "https://res.cloudinary.com/ajithvault/image/upload/v1575547105/BLR_CM%20Reddy/BLR_CM_Reddy_Res_Int_01_ncz15v.jpg",
+  ""]
+
+  }
+];
