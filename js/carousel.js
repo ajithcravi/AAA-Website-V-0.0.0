52772dad78d7ab860a5913a987001b505444d227
ajithcravi <craviajith@gmail.com>
2019-12-05 09:55:58 +0530

    Project tiles

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,13 @@
 // This flag is declared to check whether number of times 'addImagesToCarousel' funnction is run. If it is the first time, active class should be added to the dots and carousel images
 let flag = 0;
+let carouselImageData = [];
 
 //Fetch the details of the images to be used in main carousel from the cloudinary cdn
 fetch(
   "http://res.cloudinary.com/ajithvault/raw/upload/v1575472700/AAA/Data/carouselImages_kzwtiy.json"
 )
   .then(response => response.json())
-  .then(data => localStorage.setItem("carouselData", JSON.stringify(data)));
-
-let carouselImageData = JSON.parse(localStorage.getItem("carouselData"));
+  .then(data => (carouselImageData = JSON.stringify(data)));
 
 // This function adds images to the carouselImageData. This is called in the array.map function
 addImagesToCarousel = object => {
